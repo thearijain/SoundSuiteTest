b2a07d4abd57be05391c952c9e77e2c8142219d5
Arihant Jain <thearijain@gmail.com>
2022-06-25 21:17:25 -0400

    Initial Commit

---
+++ SoundSuiteTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoundSuiteTest
+//
+//  Created by Ari Jain on 6/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
